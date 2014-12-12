d9c6ce8a0dfd003caf776a1259251b18afef6904
CodingHouse <sb@codinghouse.co>
2014-12-12 12:57:28 -0800

    Add resources domain and offer

--- db/migrate/20141212205507_create_domains.rb
+++ db/migrate/20141212205507_create_domains.rb
@@ -1,8 +1,10 @@
-class AddDomainsTable < ActiveRecord::Migration
+class CreateDomains < ActiveRecord::Migration
   def change
     create_table :domains do |t|
       t.string :domain_name, null: false
       t.string :owner_email, null: false
+
+      t.timestamps
     end
   end
 end
