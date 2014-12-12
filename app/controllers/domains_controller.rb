9d3f7c0611d5b293b9aecb160959e381b9661305
CodingHouse <sb@codinghouse.co>
2014-12-12 14:28:58 -0800

    clear the puts

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -5,9 +5,6 @@ class DomainsController < ApplicationController
   end
 
   def create # domains#create
-    puts "we're in business"
-    puts params[:domain]
-    puts params[:email]
     Domain.create domain_name: params[:domain], owner_email: params[:email]
     flash[:notice] = "Success"
     redirect_to "/"
