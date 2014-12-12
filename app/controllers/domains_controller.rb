fc174fdf2dded5bcd3dcab860fdf874cc61b6e13
CodingHouse <sb@codinghouse.co>
2014-12-12 13:40:21 -0800

    Add @domain var

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -1,6 +1,12 @@
 class DomainsController < ApplicationController
-  def create #domains#create
+
+  def new
+    @domain = Domain.new
+  end
+
+  def create # domains#create
     puts "we're in business"
     redirect_to "/"
   end
+
 end
